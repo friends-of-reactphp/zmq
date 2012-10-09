0312492cc11f0500e8590fce701365b6480947b1
Igor Wiedler <igor@wiedler.ch>
2012-10-09 23:41:16 +0200

    Use EventLoop factory

--- examples/http-server/server.php
+++ examples/http-server/server.php
@@ -2,7 +2,7 @@
 
 require 'vendor/autoload.php';
 
-$loop = new React\EventLoop\StreamSelectLoop();
+$loop = React\EventLoop\Factory::create();
 $socket = new React\Socket\Server($loop);
 $http = new React\Http\Server($socket);
 
