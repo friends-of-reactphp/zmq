ddd223ffd97ceff96b31c0d4964dc2e83004b8e7
Romain Neutron <imprec@gmail.com>
2012-11-14 23:01:45 +0100

    Fix pub/sub use

--- examples/pubsub.php
+++ examples/pubsub.php
@@ -7,7 +7,7 @@ $loop = React\EventLoop\Factory::create();
 $context = new React\ZMQ\Context($loop);
 
 $sub = $context->getSocket(ZMQ::SOCKET_SUB);
-$sub->bind('tcp://127.0.0.1:5555');
+$sub->connect('tcp://127.0.0.1:5555');
 $sub->subscribe('foo');
 
 $sub->on('message', function ($msg) {
@@ -15,7 +15,7 @@ $sub->on('message', function ($msg) {
 });
 
 $pub = $context->getSocket(ZMQ::SOCKET_PUB);
-$pub->connect('tcp://127.0.0.1:5555');
+$pub->bind('tcp://127.0.0.1:5555');
 
 $i = 0;
 $loop->addPeriodicTimer(1, function () use (&$i, $pub) {
